3b9ccfed13a14463c5df76c1901a275c855e0e9d
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-10-23 16:14:34 +0300

    Test commit

--- AnimeWebApp/Controllers/HomeController.cs
+++ AnimeWebApp/Controllers/HomeController.cs
@@ -9,5 +9,10 @@ namespace AnimeWebApp.Controllers
         {
             return View();
         }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
