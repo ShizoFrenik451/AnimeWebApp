a8d9930b4c4d5772da7496ea9515400e4a6fb41c
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-11-27 10:54:21 +0300

    add db connection

--- AnimeWebApp/Controllers/HomeController.cs
+++ AnimeWebApp/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
+using System.Linq;
+using AnimeWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace AnimeWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET
+        public readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        
         public IActionResult Index()
         {
+            //var user = _db.Users.FirstOrDefault<User>();
             return View();
         }
         
--- AnimeWebApp/Startup.cs
+++ AnimeWebApp/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnimeWebApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +19,7 @@ namespace AnimeWebApp
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -24,7 +28,25 @@ namespace AnimeWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            //services.AddDbContext<ApplicationDbContext>();
+            /*services.AddEntityFrameworkNpgsql().AddDbContextFactory<ApplicationDbContext>(options => options.UseNpgsql(
+                Configuration.GetConnectionString("DefaultConnection")
+                )
+            );*/
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
         }
+        
+        /*public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+        {
+            public ApplicationDbContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+
+                return new ApplicationDbContext(optionsBuilder.Options);
+            }
+        }*/
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
