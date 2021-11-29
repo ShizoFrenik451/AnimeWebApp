d8e31e12deb6bc1e1d1e36e59c1ddb18afbdb1c0
ShizoFrenik451 <variginegor@gmail.com>
2021-11-29 14:58:24 +0300

    Баг, оно не удаляется

--- AnimeWebApp/obj/Release/net5.0/AnimeWebApp.RazorAssemblyInfo.cs
+++ AnimeWebApp/obj/Release/net5.0/AnimeWebApp.RazorAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AnimeWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("AnimeWebApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AnimeWebApp")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("AnimeWebApp.Views")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
